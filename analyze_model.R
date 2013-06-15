35b4c7dcdc35523cf30bea6731f3a69d3626c3d2
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-06-15 14:14:44 -0400

    refactor model choice

--- analyze_model.R
+++ analyze_model.R
@@ -1,6 +1,6 @@
-import_model <- function(path) { 
-    wkf <- read.csv(file.path(path,"keys.csv"))
-    doctops <- read.csv(file.path(path,"doc_topics.csv"),as.is=T)
+import_model <- function(keys,doctops) { 
+    wkf <- read.csv(keys)
+    doctops <- read.csv(doctops,as.is=T)
 
     list(wkf=wkf,
          doctops=doctops) 
@@ -8,7 +8,7 @@ import_model <- function(path) {
 }
 
 # main()
-analyze_model_main <- function() {
+analyze_model_main <- function(model="test") {
     source("~/Developer/dfr-analysis/source_all.R")
 
     # parameters
@@ -23,14 +23,22 @@ analyze_model_main <- function() {
                   "res1981-2012")
     dfr_dirs <- file.path(dfr_data_root,journal_dirs)
 
-    model_dir <- "/Users/agoldst/Documents/research/20c/hls/tmhls/models/out130612/"
-
-    testing <- F
-    # uncomment to run on small test set
-    # testing <- T
-
-    if(testing) {
+    if(model=="130612") { 
+        model_dir <- "/Users/agoldst/Documents/research/20c/hls/tmhls/models/out130612/"
+        doctops_file <- file.path(model_dir,"doc_topics.csv")
+        keys_file <- file.path(model_dir,"keys.csv")
+    } else if(model=="130611") { 
+        model_dir <- "/Users/agoldst/Documents/research/20c/hls/tmhls/models/out130611/"
+        # slightly different filename on 130611 model
+        doctops_file <- gzfile(file.path(model_dir,"topics.csv.gz"))
+        keys_file <- file.path(model_dir,"keys.csv")
+    } else if(model=="test") {
         model_dir <- "/Users/agoldst/Documents/research/20c/hls/tmhls/models/test/"
+        keys_file <- file.path(model_dir,"keys.csv")
+        doctops_file <- file.path(model_dir,"topics.csv")
+    }
+    else {
+        stop("Specify a model to analyze.")
     }
 
     report_dir <- file.path(model_dir,"report")
@@ -40,7 +48,6 @@ analyze_model_main <- function() {
 
     boxplot_time <- "10 years"
 
-
     # do the analysis
 
     message("Loading metadata")
@@ -49,7 +56,7 @@ analyze_model_main <- function() {
 
     message("Loading modeling results")
 
-    model <- import_model(model_dir)
+    model <- import_model(keys_file,doctops_file)
 
     message("Converting document-topic matrix to long form") 
 
@@ -67,6 +74,11 @@ analyze_model_main <- function() {
 }
 
 # execution
+# choose which model (file paths specified in main() subroutine)
 
-results <- analyze_model_main()
+results <- analyze_model_main(
+                              model="130612"
+                              # model="130611"
+                              # model="test"
+                              )
 
