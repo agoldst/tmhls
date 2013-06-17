a26386ec16cea4e45322c9779d06c108af711889
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-06-17 14:15:43 -0400

    refactoring of make_model
    sourcing this file no longer executes mallet. instead, you get a
    function make_model(), which you should call yourself.
    
    All parameters set as variables in the former make_model_main() are now
    parameters to make_model()

--- make_model.R
+++ make_model.R
@@ -1,52 +1,39 @@
 
-make_model_main <- function() {
-    source("~/Developer/dfr-analysis/source_all.R")
-
-    output_dir <- "/spare2/ag978/130612/"
-
-    modeling_params <- list(
-        instances="/spare2/ag978/130612/journals.mallet",
-        num.topics=150,
+make_model <- function(
+        output_dir,
+        num.topics,
+        instances=file.path(output_dir,"journals.mallet"),
         alpha.sum=5,
         beta=0.01 ,
         n.iters=500,
         n.max.iters=10,
         n.hyper.iters=20,
         n.burn.in=50,
-        threads=16L)
+        threads=16L,
+        smoothed=F,         # applies to doc-topic and topic-word
+        normalized=F,       # ditto
+        doctopics_file=file.path(output_dir,"doc_topics.csv"),
+        state_file=file.path(output_dir,"mallet_state.gz"),
+        num.top.words=50,   # number of top "key" words for each topic
+        wk_file=file.path(output_dir,"keys.csv"),
+        topic_words_file=file.path(output_dir,"topic_words.csv"),
+        vocab_file=file.path(output_dir,"vocab.txt"),
+        dfr_analysis_source="~/Developer/dfr-analysis/source_all.R") {
 
-    testing <- F 
-    # uncomment to use fast test settings
-    # testing <- T
-    if(testing) {
-        modeling_params$instances <- "/Users/agoldst/Documents/research/20c/hls/tmhls/models/test/journals.mallet"
-        modeling_params$n.iters <- 200
-        modeling_params$num.topics <- 10
-        modeling_params$threads <- 2L
-        output_dir <- "/Users/agoldst/Documents/research/20c/hls/tmhls/models/test"
-    }
+    source(dfr_analysis_source)
 
-    # output parameters
-
-    # smoothing/normalization for document-topic and topic-word tables?
-    smoothed <- T
-    normalized <- T
-
-
-    doctopics_file <- file.path(output_dir,"doc_topics.csv")
-    state_file <- file.path(output_dir,"mallet_state.gz")
-
-    # how many "top key words" for each topic?
-    num.top.words <- 50
-    wk_file <- file.path(output_dir,"keys.csv")
-
-    # two files for all topic-word assignments
-    topic_words_file <- file.path(output_dir,"topic_words.csv")
-    vocab_file <- file.path(output_dir,"vocab.txt")
-    
     message("Beginning mallet train-topics run...")
 
-    trainer <- do.call(train_model,modeling_params)
+    trainer <- train_model( 
+            instances=instances,
+            num.topics=num.topics,
+            alpha.sum=alpha.sum,
+            beta=beta,
+            n.iters=n.iters,
+            n.max.iters=n.max.iters,
+            n.hyper.iters=n.hyper.iters,
+            n.burn.in=n.burn.in,
+            threads=threads)
 
     message("mallet run complete.")
     message("Saving document topics to ",doctopics_file)
@@ -82,7 +69,6 @@ make_model_main <- function() {
 }
 
 # execution: should allow the trainer object to persist
-
-trainer <- make_model_main()
+# as in: trainer <- make_model()
 
 # TODO diagnostics
