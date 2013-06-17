421c8d98e67e543b56aa35e5bc613e1711bafc0c
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-06-17 14:00:54 -0400

    refactoring of make_instance, more corpus-cleaning
    incorporates code to translate britticisms
    
    incorporates a length minimum
    
    factors all parameters formerly set as variables in make_instance_main()
    into actual parameters of make_instance()
    
    Sourcing this file no longer executes anything. Write a script to call
    make_instance_main(),specifying any non-default parameters.

--- make_instance.R
+++ make_instance.R
@@ -1,15 +1,40 @@
-prune_filelist <- function(files,metadata,aquo,adquem,types="fla\t") {
+
+# TU from make_instance_nobrit.R: I edited this function to permit
+# exclusions of MLR reviews misclassified as "fla."
+#
+# AG addition: let's do it functionally. exclude() should operate on
+# metadata and return a vector of ids
+
+prune_filelist <- function(files,metadata,aquo,adquem,types="fla\t",
+                           exclude=function (metadata) { NULL }) {
     # apply cutoff dates
     keep_ids <- metadata$id[metadata$date >= as.Date(aquo) &
                             metadata$date <= as.Date(adquem) &
                             metadata$type %in% types]
+    # apply further exclusions
+    keep_ids <- keep_ids[!keep_ids %in% exclude(metadata)]
     files[as.id(files) %in% keep_ids]
 }
 
-                     
-get_counts <- function(dirs,
-                       aquo,adquem,
-                       itemtypes) {
+
+mlr_review_exclude <- function(metadata) {
+    with(metadata,
+         id[type=="fla\t" &
+            title=="Review\t" &
+            journaltitle=="The Modern Language Review\t"]
+         )
+}
+
+exclude_shorts <- function(metadata,document_lengths,threshold) {
+    document_lengths$id[document_lengths$length < threshold]
+}
+
+# AG: following TU's changes, allow a list of files with exclusions
+#
+# Unlike make_instance_nobrit, here we expect files with just id's, one
+# per line
+
+get_counts <- function(dirs,aquo,adquem,itemtypes,exclude) {
 
     message("Loading metadata...")
 
@@ -22,13 +47,47 @@ get_counts <- function(dirs,
     files <- prune_filelist(files=Sys.glob(globs),
                             metadata=metadata,
                             aquo=aquo,adquem=adquem,
-                            types=itemtypes)
+                            types=itemtypes,
+                            exclude=exclude)
 
     message("Importing ",length(files)," wordcount.CSV files")
 
     read_dfr(files=files)
 }
  
+read_britticisms <- function(filepath) {
+    aframe <- read.csv(filepath, stringsAsFactors = FALSE)
+    trans_table <- aframe$AMERICAN
+    names(trans_table) <- aframe$BRITISH
+    trans_table
+}
+
+# translate_britticisms
+#
+# Accepts a long-form dataframe as returned by read_dfr with
+# three columns, id, WORDCOUNTS, and WEIGHT.
+#
+# Also accepts a named vector used as a translation table,
+# where the contents of the vector are American spellings
+# and the names are British spellings. Using this, translates
+# British items in WORDCOUNTS into corresponding American
+# spellings. Does not attempt to merge duplicate rows of the
+# data frame which may be created (e.g. when colour -> color,
+# you may end up with two rows for "color.") Assumes that those
+# rows will effectively be merged when wordcounts are inflated
+# into actual text for Mallet by docs_frame or a similar function.
+#
+# Probably not very fast because named vectors in R are not
+# implemented as hash tables like Python dictionaries. But it's
+# not a bottleneck in practice.
+
+translate_britticisms <- function(counts, american_translations) {
+    british_spellings <- names(american_translations)
+    brits <- which(counts$WORDCOUNTS %in% british_spellings) 
+    counts$WORDCOUNTS[brits] <- american_translations[counts$WORDCOUNTS[brits]]
+    counts
+}
+
 rare_token_report <- function(overall,
                               freq_threshold=NULL,rank_threshold=NULL,
                               plotsfile="freqplots.png") {
@@ -97,67 +156,89 @@ stopword_report <- function(overall,stoplist_file) {
             sprintf("%.3f",stopcount / total),") in the corpus")
 }
 
-# main()
+# make_instance: main function
 
-make_instance_main <- function() {
+make_instance <- function(
+        outdir,
+        dfr_analysis_source="~/Developer/dfr-analysis/source_all.R",
+        tmhls_root="~/Documents/research/20c/hls/tmhls",
+        dfr_data_root=file.path(tmhls_root,"dfr-data"),
+        journal_dirs=c("elh_ci_all",
+                       "mlr1905-1970",
+                       "mlr1971-2013",
+                       "modphil_all",
+                       "nlh_all",
+                       "pmla_all",
+                       "res1925-1980",
+                       "res1981-2012"),
+        aquo=as.Date("1880-01-01"),
+        adquem=as.Date("2013-12-31"),
+        itemtypes="fla\t",
+        exclude=mlr_review_exclude,                     # a function
+        length_min=1000,                                # words
+        lengths_outfile=file.path(outdir,"document_lengths.csv"),
+        britticisms_file=file.path(tmhls_root,"UK2UStransrules.csv"),
+        freq_threshold=NULL,
+        rank_threshold=100000, 
+        plotfile=file.path(outdir,"freqplots.png"),
+        outfile=file.path(outdir,"journals.mallet")) {
+
     # "includes"
-    source("~/Developer/dfr-analysis/source_all.R")
+    source(dfr_analysis_source)
     library(ggplot2)
     library(grid)
-    pwd <- getwd()
-    setwd("~/Documents/research/20c/hls/tmhls")
 
-    # parameters
-    dfr_data_root <- "/Users/agoldst/Documents/research/20c/hls/tmhls/dfr-data"
-    journal_dirs <- c("elh_ci_all",
-                  "mlr1905-1970",
-                  "mlr1971-2013",
-                  "modphil_all",
-                  "nlh_all",
-                  "pmla_all",
-                  "res1925-1980",
-                  "res1981-2012")
+    # main script: commands
+
     dfr_dirs <- file.path(dfr_data_root,journal_dirs)
 
-    outdir <- "/Users/agoldst/Documents/research/20c/hls/tmhls/models/out130612/"
-    # or for faster testing, uncomment:
-    # dfr_dirs <- "~/Developer/dfr-analysis/test_data/pmla_sample"
-    # outdir <- "/Users/agoldst/Documents/research/20c/hls/tmhls/models/test"
-
-    aquo <- as.Date("1905-01-01")
-    adquem <- as.Date("2004-12-31")
-    itemtypes <- "fla\t"
-    
-    # corpus-trimming parameters
-    # this should be pretty moderate
-    # freq_threshold <- 1e-7
-    #rank_threshold <- NULL
-    # this would be more aggressive
-    freq_threshold <- NULL
-    rank_threshold <- 10000
-
-    
-
-    plotfile <- file.path(outdir,"freqplots.png")
-    outfile <- file.path(outdir,"journals.mallet")
+    pwd <- getwd()
+    setwd(tmhls_root)
+    message("wd now: ",tmhls_root)
 
     message("regenerating stoplist_final.txt") 
     system("python stoplist_final.py",ignore.stdout=T,ignore.stderr=T)
 
-    stoplist_file <- "stoplist_final.txt"
-
-    # main script: commands
+    stoplist_file <- file.path(tmhls_root,"stoplist_final.txt")
 
     if(!file.exists(outdir)) {
       dir.create(outdir)
     }
 
+    if(length_min > 0) {
+        wc_dirs <- file.path(dfr_dirs,"wordcounts")
+        pythonscript <- file.path(tmhls_root,"document_lengths.py")
+        cmd <- paste("python",pythonscript,paste(wc_dirs,collapse=" "))
+        cmd <- paste(cmd,">",lengths_outfile)
+        message("Running ",cmd)
+        system(cmd)
+        lengths <- read.csv(lengths_outfile,as.is=T)
+        lengths$id <- as.id(lengths$filename)
+
+        excluder <- function(metadata) {
+            c(exclude(metadata),exclude_shorts(metadata,lengths,length_min))
+        }
+    }
+    else {
+        excluder <- exclude
+    }
+
+    setwd(pwd)
+    message("wd now: ",pwd)
+
     counts <- get_counts(dfr_dirs,
                          aquo,
                          adquem,
-                         itemtypes)
+                         itemtypes,
+                         exclude=excluder)
 
     message("Read ",nrow(counts)," rows") 
+
+    message("'Normalizing' British spellings to American...")
+
+    britticisms <- read_britticisms(britticisms_file)
+    counts <- translate_britticisms(counts,britticisms)
+
     message("Aggregating token counts into overall counts")
 
     overall <- overall_counts(counts) 
@@ -176,9 +257,4 @@ make_instance_main <- function() {
     write_instances(inst,outfile) 
     message("Instance saved to ",outfile)
 
-    setwd(pwd)
 }
-
-# execution
-
-make_instance_main()
