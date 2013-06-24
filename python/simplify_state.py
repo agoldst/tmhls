9fff9c4ab221b5ba1aaa13352cea2515bb552b77
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-06-24 08:41:02 -0400

    Head comment

--- python/simplify_state.py
+++ python/simplify_state.py
@@ -3,8 +3,19 @@ import sys
 
 from collections import defaultdict
 
+# Take a mallet state.gz file and produce a csv of doc-type-topic
+# counts. Output to stdout.
+#
+# USAGE: python simplify_state.py state.gz > state_tallied.csv
+#
+# The mallet state is saved in a gz file with lots of redundant
+# information. For our purposes, we don't know the order of tokens in
+# documents, so we can "reduce" the state information to a list of
+# 4-tuples: doc,type,topic,count.
+
 def process_file(state_file):
 
+    # utility function for printing our type-topic tallies as we go
     def output_tally(doc,tally):
         for typeindex in tally:
             for topic in tally[typeindex]:
@@ -18,6 +29,7 @@ def process_file(state_file):
         for line in f:
             if line[0] == "#":
                 continue
+
             # We can assume that the state is written out doc-by-doc,
             # so we only need to count up type-topic pairs until we get
             # to the end of the state for a given doc
