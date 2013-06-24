fb57c024461c6bbb3a28b96460742022893010f6
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-06-24 11:05:40 -0400

    wrappers for making per-topic term-year matrices

--- analyze_model.R
+++ analyze_model.R
@@ -49,9 +49,7 @@ model_files <- function(model) {
              keys_file=file.path(model_dir,"wkf_nosmooth.csv"),
              model_smoothed=F,
              model_normalized=F,
-             generate_report=F,
-             instances_file=path.expand("~/Documents/research/20c/hls/tmhls/models/hls_k48_v100K/journals.mallet"),
-             generate_tym=T)
+             generate_report=F)
     } 
     else {
         stop("Specify a model to analyze.")
@@ -73,7 +71,11 @@ import_model <- function(keys,doctops) {
 
 # analyze_model
 #
-# main function
+# main function for loading results of the topic modeling run. Also
+# saves a file with just the head words for topics
+#
+# if generate_report=T (default), also generates a folder of plots
+# giving topic overviews
 
 analyze_model <- function(
         model_dir,
@@ -81,9 +83,6 @@ analyze_model <- function(
         keys_file=file.path(model_dir,"keys.csv"),
         report_dir=file.path(model_dir,"report"),
         keys_summary_file=file.path(report_dir,"keys_summary.csv"),
-        generate_tym=F,
-        instances_file=file.path(model_dir,"journals.mallet"),
-        tym_result_file=file.path(report_dir,"tym_result.rda"), 
         dfr_analysis_root="~/Developer/dfr-analysis",
         dfr_analysis_source=file.path(dfr_analysis_root,"source_all.R"),
         dfr_data_root="~/Documents/research/20c/hls/tmhls/dfr-data" ,
@@ -108,10 +107,6 @@ analyze_model <- function(
     topics_rmallet_setup()
     setwd(analyze_model_wd)
 
-    # parameters
-
-
-
     if(!file.exists(report_dir)) {
         dir.create(report_dir)
     }
@@ -119,15 +114,12 @@ analyze_model <- function(
     # do the analysis
 
     message("Loading metadata")
-
     metadata <- read_metadata(file.path(dfr_dirs,"citations.CSV"))
 
     message("Loading modeling results")
-
     model <- import_model(keys_file,doctops_file)
 
     message("Converting document-topic matrix to long form") 
-
     model$dtl <- doc_topics_long(model$doctops,metadata,
                                  meta_keep="pubdate")
     dt_wide <- merge(model$doctops,metadata[,c("pubdate","id")],by="id")
@@ -150,16 +142,18 @@ analyze_model <- function(
 
     write.csv(wkf_kf(model$wkf),keys_summary_file,
               quote=F,row.names=F)
-
     message("Saved summary of top key words to ",keys_summary_file)
 
     result <- list(model=model,metadata=metadata)
 
-
     # allow results to persist
-
     result
 }
+
+# generate_tym
+#
+# Save the term-year matrix for the corpus that was modeled to a file, in 
+# native R format.
 
 generate_tym <- function(
         instances_file,
@@ -197,4 +191,113 @@ generate_tym <- function(
                                    instances=instances)
     message("Saving result list to ",tym_result_file) 
     save(tym_result,file=tym_result_file) 
+}
+
+write_corpus_info_files <- function(model_dir,
+        instances_file=file.path(model_dir,"journals.mallet"),
+        vocab_file=file.path(model_dir,"vocab.txt"),
+        id_map_file=file.path(model_dir,"id_map.txt")) {
+
+    message("Reloading instances from ",instances_file)
+    instances <- read_instances(instances_file)
+
+    id_map <- instances_ids(instances)
+    vocab <- instances_vocabulary(instances)
+    instances <- read_instances(instances_file)
+
+    message("Saving vocabulary to ",vocab_file)
+    writeLines(vocab,vocab_file)
+
+    message("Saving document id list to ",id_map_file)
+    writeLines(id_map,id_map_file)
+}
+
+read_corpus_info_files <- function(model_dir,
+        vocab_file=file.path(model_dir,"vocab.txt"),
+        id_map_file=file.path(model_dir,"id_map.txt")) {
+    vocab <- readLines(vocab_file)
+    id_map <- readLines(id_map_file)
+
+    list(vocab,id_map)
+}
+
+
+# generate_tytm
+#
+# Save matrices for occurrences of words in a given topic per year, in R
+# native format.
+#
+# topics: the function will save one file per topic, holding a single
+# list called tytm_result. The files aren't small although they *should*
+# be sparseMatrices. If you load more than one into the same environment
+# you'll clobber the tytm_result objects and be left only with the last
+# one you loaded. arRgh.
+
+generate_tytm <- function(
+        topics,
+        ss,         # the "simplified state"--see load_ss() below
+        model_dir,
+        tytm_file_template=file.path(model_dir,"tytm%03d.rda"), # for sprintf
+        vocab_file=file.path(model_dir,"vocab.txt"),
+        id_map_file=file.path(model_dir,"id_map.txt"),
+        dfr_data_root="~/Documents/research/20c/hls/tmhls/dfr-data" ,
+        journal_dirs=c("elh_ci_all",
+                       "mlr1905-1970",
+                       "mlr1971-2013",
+                       "modphil_all",
+                       "nlh_all",
+                       "pmla_all",
+                       "res1925-1980",
+                       "res1981-2012"),
+        dfr_dirs=file.path(dfr_data_root,journal_dirs),
+        dfr_analysis_root="~/Developer/dfr-analysis",
+        dfr_analysis_source=file.path(dfr_analysis_root,"source_all.R")) {
+
+    analyze_model_wd <- getwd()
+    setwd(dfr_analysis_root)
+    source(dfr_analysis_source)
+    topics_rmallet_setup()
+    setwd(analyze_model_wd)
+
+    message("Reading metadata") 
+    metadata <- read_metadata(file.path(dfr_dirs,"citations.CSV"))
+    id_map <- readLines(id_map_file)
+    vocab <- readLines(vocab_file)
+
+
+    for(topic in topics) {
+        tytm_file <- sprintf(tytm_file_template,topic)
+
+        message("Getting the term-year matrix for topic ",topic)
+        tytm_result <- term_year_topic_matrix(topic,ss,id_map,metadata,vocab)
+        
+        save(tytm_result,file=tytm_file) 
+        message("Saved the results in R native format to ",tytm_file)
+    }
+}
+
+# load_ss
+#
+# load the "simplified state," i.e. the Gibbs sampling state aggregated into
+# doc-word-topic counts. Returns a reference to a big.matrix.
+
+load_ss <- function(
+        ss_file=file.path(model_dir,"state_simple.csv"),
+        state_file=file.path(model_dir,"mallet_state.gz"),
+        ss_script="python/simplify_state.py") {
+
+    if(!file.exists(ss_file)) {
+        message("Simplified state file ",ss_file," not found. It will be regenerated, then loaded.")
+        generate_ss <- T
+    }
+    else {
+        generate_ss <- F
+    }
+
+    ss <- read_simplified_state(infile=ss_file,
+                                generate_file=generate_ss,
+                                state_file=state_file,
+                                simplifier=ss_script,
+                                big=T)
+    ss
 }
