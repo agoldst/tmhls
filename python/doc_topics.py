942e7a42396f88bdd674a43d44732b46d00c63ad
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-08-20 18:01:31 -0400

    correct usage comment

--- python/doc_topics.py
+++ python/doc_topics.py
@@ -3,9 +3,10 @@ import sys
 from collections import defaultdict
 
 # Take a simplified-mallet-state file (made by simplify_state.py) and
-# produce a csv of the doc-topic counts matrix. Output to stdout.
+# produce a csv of the doc-topic counts matrix. Output to stdout. Specify the 
+# number of topics as the second argument to the command.
 #
-# USAGE: python doc_topics.py state_simple.csv > document_topics.csv
+# USAGE: python doc_topics.py state_simple.csv 100 > document_topics.csv
 #
 # The results should be consistent with the matrix saved by
 # doc_topics_frame() in R. N.B. that the results of this script have no
