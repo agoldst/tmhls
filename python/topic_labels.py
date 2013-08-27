935a27ba2a0c8f3f9937bd319d569d07a0c28718
Andrew Goldstone <andrew.goldstone@gmail.com>
2013-08-27 12:54:17 -0400

    topic-label macro

--- python/topic_labels.py
+++ python/topic_labels.py
@@ -1,11 +1,29 @@
+# topic_labels.py
+# generate a tex macro for labeling topics
+#
+# usage:
+# python topic_labels.py path/to/labels.txt [n]
+#
+# labels.txt should have lines of the form
+#
+# NN word1 word2 word3 ...
+# 
+# where NN is the number of the topic
+
 def main(script,lfile,n="3"):
     n_words = int(3)
+    print r'''
+\def\topiclabel#1{
+    \ifcase#1
+        no topic zero'''
     with open(lfile) as f: 
         for line in f:
             fields = line.strip().split(' ')
+            print r'\or'
             label = " ".join(fields[1:n_words + 1])
-            macro = r'\topiclabel{' + fields[0] + '}{' + label + '}'
-            print r'\newcommand{\topic' + fields[0] + "}{" + macro + "}"
+            print r'\topicformat{' + fields[0] + "}{" + label + "}"
+        
+    print r'\fi }'
 
 if __name__=='__main__':
     import sys
